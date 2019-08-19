e85efc7ead157431314ab8b3459f01d9c6f5c5f4
aptmac <almacdon@redhat.com>
2019-08-19 13:53:10 -0400

    use Jemmy supplied by Maven Central (#2)
    Comment-out the Jemmy setup script before running the uitests

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@
 bash $(dirname "$0")/1_setup_jmc.sh || { echo "Failed to build JMC."; exit 1; };
 
 # Clone the Jemmy repo, build it, and move the resulting jars into jmc
-bash $(dirname "$0")/2_setup_jemmy.sh || { echo "Failed to setup Jemmy."; exit 1; };
+# bash $(dirname "$0")/2_setup_jemmy.sh || { echo "Failed to setup Jemmy."; exit 1; };
 
 # Run the ui tests
 bash $(dirname "$0")/3_run_ui_tests.sh || { echo "Failed to run UI tests."; exit 1; };
